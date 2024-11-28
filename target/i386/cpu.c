f080abf5020f0cfc6cb0716b9cf64aa4de36f348
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-11-28 17:49:07 -0500

    Update cpu.c

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -5853,7 +5853,12 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
     default:
         #if 1
         if (tcg_enabled()) {
-                host_cpuid(index, count, eax, ebx, ecx, edx);
+            host_cpuid(index, count, eax, ebx, ecx, edx);
+        } else {
+             *eax = 0;
+             *ebx = 0;
+             *ecx = 0;
+             *edx = 0;
         }
         #else
         *eax = 0;
