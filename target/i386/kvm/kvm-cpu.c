9e12473dafed85a64e277f92b0133dacaa66cbb9
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-12-31 15:31:52 -0500

    Update kvm-cpu.c

--- target/i386/kvm/kvm-cpu.c
+++ target/i386/kvm/kvm-cpu.c
@@ -128,13 +128,6 @@ static void kvm_cpu_instance_init(CPUState *cs)
     host_cpu_instance_init(cpu);
 
     if (xcc->model) {
-        /* only applies to builtin_x86_defs cpus */
-        if (!kvm_irqchip_in_kernel()) {
-            x86_cpu_change_kvm_default("x2apic", "off");
-        } else if (kvm_irqchip_is_split()) {
-            x86_cpu_change_kvm_default("kvm-msi-ext-dest-id", "on");
-        }
-
         /* Special cases not set in the X86CPUDefinition structs: */
         x86_cpu_apply_props(cpu, kvm_default_props);
     }
