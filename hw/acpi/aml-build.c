50c3d29d0bcfbf7651d4e5f8b6e8c6e129a7b86c
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-01-14 17:27:31 -0500

    Update aml-build.c

--- hw/acpi/aml-build.c
+++ hw/acpi/aml-build.c
@@ -1728,7 +1728,7 @@ void acpi_table_begin(AcpiTable *desc, GArray *array)
     /* OEM Table ID */
     build_append_padded_str(array, ACPI_BUILD_APPNAME8, 8, '\0'); //desc->oem_table_id
     build_append_int_noprefix(array, 1, 4); /* OEM Revision */
-    g_array_append_vals(array, "PTL ", 4); /* Creator ID */
+    g_array_append_vals(array, "CECL", 4); /* Creator ID */
     build_append_int_noprefix(array, 1, 4); /* Creator Revision */
 }
 
